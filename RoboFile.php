754a6a900215ba4cfa236178b67e2379af11d7c4
Anderson Grudtner Martins <anderson@ostraining.com>
2017-11-10 18:33:22 -0200

    Add test methods to the robo file

--- RoboFile.php
+++ RoboFile.php
@@ -9,8 +9,45 @@ require 'vendor/autoload.php';
  */
 class RoboFile extends PressShack\Builder\Robo_Task {
 
-    public function __construct() {
-        $this->plugin_name      = 'publishpress';
-        $this->version_constant = 'PUBLISHPRESS_VERSION';
-    }
+	public function __construct() {
+		$this->plugin_name      = 'publishpress';
+		$this->version_constant = 'PUBLISHPRESS_VERSION';
+	}
+	
+	public function testUnit() {
+		$return = $this->taskCodecept()
+			->suite('unit')
+			->html('report_unit.html')
+			->run();
+
+		return $return;
+	}
+
+	public function testWPUnit() {
+		$this->taskCodecept()
+			->suite('wpunit')
+			->html('report_wpunit.html')
+			->run();
+	}
+
+	public function testFunctional() {
+		$this->taskCodecept()
+			->suite('functional')
+			->html('report_functional.html')
+			->run();
+	}
+
+	public function testAcceptance() {
+		$this->taskCodecept()
+			->suite('acceptance')
+			->html('report_acceptance.html')
+			->run();
+	}
+
+	public function testAll() {
+		$this->testUnit();
+		$this->testWPUnit();
+		$this->testFunctional();
+		$this->testAcceptance();
+	}
 }
